3d2a65edb207e04458322e7b11de89fc9a6b404a
amcd13 <141310954+amcd13@users.noreply.github.com>
2023-08-07 14:19:31 +0800

    Update __init__.py

--- pfgadgets/__init__.py
+++ pfgadgets/__init__.py
@@ -5,6 +5,7 @@ from pfgadgets.pfgadgets import FrequencySweep
 from pfgadgets.pfgadgets import GetData
 from pfgadgets.pfgadgets import GetObject
 from pfgadgets.pfgadgets import GetPlot
+from pfgadgets.pfgadgets import GetResult
 from pfgadgets.pfgadgets import HarmonicLoadFlow
 from pfgadgets.pfgadgets import LoadFlow
 from pfgadgets.pfgadgets import ShortCircuit
