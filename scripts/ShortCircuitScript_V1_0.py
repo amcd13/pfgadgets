8c922ee4e0ee45c1f55160520327e9e6108e3e86
amcd13 <141310954+amcd13@users.noreply.github.com>
2023-08-09 10:20:44 +0800

    Update ShortCircuitScript_V1_0.py

--- scripts/ShortCircuitScript_V1_0.py
+++ scripts/ShortCircuitScript_V1_0.py
@@ -13,64 +13,64 @@ Dependencies: pfgadgets module
 Description: This script excutes a short-circuit calculation and exports
              the results to csv files
 
-Inputs: faultTypes - The types of faults to run
+Inputs: fault_types - The types of faults to run
         maximum - The name of the maximum fault operation scenario
         minimum - The name of the minimum fault operation scenario
-        scSetName - The name of the set containing the objects to run the short-circuit calcualtion on
-        sc3pscAttributes - A list of variables to extract during 3-phase faults
-        scSpgfAttributes - A list of variables to extract during ground faults
+        sc_set_name - The name of the set containing the objects to run the short-circuit calcualtion on
+        sc3p_attributes - A list of variables to extract during 3-phase faults
+        scgf_attributes - A list of variables to extract during ground faults
 """
 
-#Get PowerFactory application
+# Get PowerFactory application
 app = pf.GetApplication()
 app.EchoOff()
 app.ClearOutputWindow()
 
-######### INPUTS ############################################
+#############################################################
+# INPUTS
 
-#Define fault types to run
-faultTypes = ['3psc','spgf']
+# Define fault types to run
+fault_types = ['3psc', 'spgf']
 
-#Define maximum and minimum operation scenarios
+# Define maximum and minimum operation scenarios
 maximum = ['Operation Scenario']
 minimum = ['Operation Scenario']
 
-#Setup set names
-scSetName = 'Short-Circuit Set'
+# Setup set names
+sc_set_name = 'Short-Circuit Set'
 
-#Setup object attributes to collect from load flow results
-sc3pscAttributes = ['m:Ikss','m:Skss']
-scSpgfAttributes = ['m:Ikss:A','m:Skss:A']
+# Setup object attributes to collect from load flow results
+sc3p_attributes = ['m:Ikss', 'm:Skss']
+scgf_attributes = ['m:Ikss:A', 'm:Skss:A']
 
 #############################################################
 
-#Define file path for export
-filePath = os.getcwd() + '\ShortCircuitResults'
+# Define file path for export
+file_path = os.getcwd() + '\\ShortCircuitResults'
 
-#Iterate through fault types
-for faultType in faultTypes:
-    #Iterate through maximum and minimum fault cases
-    for calculate in range(0,2):
-        #Change fault calculation based on maximum or minimum case
+# Iterate through fault types
+for fault_type in fault_types:
+    # Iterate through maximum and minimum fault cases
+    for calculate in range(0, 2):
+        # Change fault calculation based on maximum or minimum case
         if calculate == 0:
-            #Execute ShortCircuit maximum command
-            ShortCircuit(scSetName, faultType=faultType, calculate=calculate, setSelect=True, opScen=maximum)
-            #Define file name for export
-            fileName = 'ShortCircuitData_%s_%s' % (faultType, 'Max')
+            # Execute ShortCircuit maximum command
+            ShortCircuit(sc_set_name, fault_type=fault_type, calculate=calculate, set_select=True, op_scen=maximum)
+            # Define file name for export
+            file_name = 'ShortCircuitData_%s_%s' % (fault_type, 'Max')
         else:
-            #Execute ShortCircuit minimum command
-            ShortCircuit(scSetName, faultType=faultType, calculate=calculate, setSelect=True, opScen=minimum)
-            #Define file name for export
-            fileName = 'ShortCircuitData_%s_%s' % (faultType, 'Min')
+            # Execute ShortCircuit minimum command
+            ShortCircuit(sc_set_name, fault_type=fault_type, calculate=calculate, set_select=True, op_scen=minimum)
+            # Define file name for export
+            file_name = 'ShortCircuitData_%s_%s' % (fault_type, 'Min')
 
-        #Collect data based on fault type
-        if faultType == '3psc':
-            #Collect objects data
-            scData = GetData(scSetName, sc3pscAttributes)
-        elif faultType == 'spgf':
-            #Collect objects data
-            scData = GetData(scSetName, scSpgfAttributes)
+        # Collect data based on fault type
+        if fault_type == '3psc':
+            # Collect objects data
+            sc_data = GetData(sc_set_name, sc3p_attributes)
+        elif fault_type == 'spgf':
+            # Collect objects data
+            sc_data = GetData(sc_set_name, scgf_attributes)
 
-        #Export data    
-        scData.export(fileName=fileName, filePath=filePath, replace=True)
-
+        # Export data
+        sc_data.export(file_name=file_name, file_path=file_path, replace=True)
