7ac9f9b4aa04f815944998c822a422da03d4f223
amcd13 <141310954+amcd13@users.noreply.github.com>
2023-08-09 10:14:45 +0800

    Update HarmonicScript_V1_0.py

--- scripts/HarmonicScript_V1_0.py
+++ scripts/HarmonicScript_V1_0.py
@@ -1,6 +1,5 @@
 import powerfactory as pf
 import os
-import pandas as pd
 from pfgadgets import HarmonicLoadFlow, FrequencySweep, GetPlot, GetObject, GetData
 
 """
@@ -19,65 +18,66 @@ Inputs: externalGridName - Name of the external grid element to modify
         terminalAttributes - List of attributes to export after harmonic load flow
 """
 
-#Get PowerFactory application
+# Get PowerFactory application
 app = pf.GetApplication()
 app.EchoOff()
 app.ClearOutputWindow()
 
-########### INPUTS #########################################
+############################################################
+# INPUTS
 
-#Define name of external grid element
-externalGridName = 'External Grid'
+# Define name of external grid element
+external_grid_name = 'External Grid'
 
-#Define names of plot pages for harmonic distortion and frequency sweep
-distortionPlotName = 'Harmonic distortion'
-frequencyPlotName = 'Frequency sweep'
+# Define names of plot pages for harmonic distortion and frequency sweep
+distortion_plot_name = 'Harmonic distortion'
+frequency_plot_name = 'Frequency sweep'
 
-#Define name of set and attributes to export data from
-terminalSetName = 'Harmonic Set'
-terminalAttributes = ['m:THD']
+# Define name of set and attributes to export data from
+terminal_set_name = 'Harmonic Set'
+terminal_attributes = ['m:THD']
 
 ############################################################
 
-#Get external grid, plot pages and title objects
-externalGrid = GetObject(externalGridName).obj
-distortionPlot = GetPlot(distortionPlotName, pageType='GrpPage')
-frequencyPlot = GetPlot(frequencyPlotName, pageType='GrpPage')
-title = distortionPlot.title
+# Get external grid, plot pages and title objects
+external_grid = GetObject(external_grid_name).obj
+distortion_plot = GetPlot(distortion_plot_name, page_type='GrpPage')
+frequency_plot = GetPlot(frequency_plot_name, page_type='GrpPage')
+title = distortion_plot.title
 
-#Clear title strings
+# Clear title strings
 title.sub1z = ''
 title.sub2z = ''
 title.sub3z = ''
 
-#Define path for export
+# Define path for export
 path = os.getcwd() + '/HarmonicResults'
 
-#Define list of max or min scenario
-maxOrMin = ['Maximum', 'Minimum']
+# Define list of max or min scenario
+max_or_min = ['Maximum', 'Minimum']
 
-#Iterrate through max and min scenarios
+# Iterate through max and min scenarios
 for i in range(2):
-    #Set external grid to max or min for harmonic purposes
-    externalGrid.SetAttribute('cusedhrm', i)
+    # Set external grid to max or min for harmonic purposes
+    external_grid.SetAttribute('cusedhrm', i)
 
-    #Define file names for plot pages export
-    harmonicFileName = 'HarmonicDistortion_%s' % (maxOrMin[i])
-    frequencyFileName = 'FrequencySweep_%s' % (maxOrMin[i])
+    # Define file names for plot pages export
+    harmonic_file_name = 'HarmonicDistortion_%s' % (max_or_min[i])
+    frequency_file_name = 'FrequencySweep_%s' % (max_or_min[i])
 
-    #Execute harmonic load flow
+    # Execute harmonic load flow
     HarmonicLoadFlow()
 
-    #Get harmonic load flow data and export
-    harmonicData = GetData(terminalSetName, terminalAttributes)
-    harmonicData.export(filePath=path, fileName=harmonicFileName, replace=True)
+    # Get harmonic load flow data and export
+    harmonic_data = GetData(terminal_set_name, terminal_attributes)
+    harmonic_data.export(file_path=path, file_name=harmonic_file_name, replace=True)
 
-    #Alter title block and export harmonic distortion plot
-    title.sub1z = maxOrMin[i] + ' Scenario'
-    distortionPlot.export(filePath=path, fileName=harmonicFileName, replace=True, frame=1)
+    # Alter title block and export harmonic distortion plot
+    title.sub1z = max_or_min[i] + ' Scenario'
+    distortion_plot.export(file_path=path, file_name=harmonic_file_name, replace=True, frame=1)
 
-    #Execute frequency sweep 
+    # Execute frequency sweep
     FrequencySweep()
 
-    #Export frequency sweep plot
-    frequencyPlot.export(filePath=path, fileName=frequencyFileName, replace=True, frame=1)
+    # Export frequency sweep plot
+    frequency_plot.export(file_path=path, file_name=frequency_file_name, replace=True, frame=1)
