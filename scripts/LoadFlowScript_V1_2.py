d8212def39da69b6d877cd7f4ea3345e58216fe7
amcd13 <141310954+amcd13@users.noreply.github.com>
2023-08-11 15:52:39 +0800

    Update LoadFlowScript_V1_3.py

--- scripts/LoadFlowScript_V1_2.py
+++ scripts/LoadFlowScript_V1_2.py
@@ -2,26 +2,26 @@ import powerfactory as pf
 import os
 import pandas as pd
 from pfgadgets import LoadFlow, GetData
+from DataProcessing_V1_0 import busbar_util, busbar_voltage, cable_util, cb_util, tx_util
 
 """
-Version: 1.2
+Version: 1.3
 
 Author: Andrew McDermott
 
 Dependencies: pfgadgets module
 
 Description: This script executes a load flow calculation and exports the
-             results to file containing csv
+             results to excel workbook
 
-Inputs: terminal_set_name - Name of the set containing calculation relevant terminal/s
+Inputs: term_set_name - Name of the set containing calculation relevant terminal/s
         line_set_name - Name of the set containing calculation relevant line/s
-        transformer_set_name - Name of the set containing calculation relevant transformer/s
-        terminal_attributes - List of variable/s to export for terminal object/s
-        line_attributes - List of variable/s to export for line object/s
-        transformer_attributes - List of variables/s to export for transformer object/s
-        terminal_headings - List of string/s to replace terminal dataframe headings
-        line_headings - List of string/s to replace line dataframe headings
-        transformer_headings - List of string/s to replace transformer dataframe headings
+        tx_set_name - Name of the set containing calculation relevant transformer/s
+        cb_set_name - Name of the set containing calculation relevant circuit breaker/s
+        term_dict - Dictionary with list of attribute/s & headings to export for terminal object/s
+        line_dict - Dictionary with list of attribute/s & headings to export for line object/s
+        tx_dict - Dictionary with list of attribute/s & headings to export for transformer object/s
+        cb_dict - Dictionary with list of attribute/s & headings to export for circuit breaker object/s
 """
 
 # Get PowerFactory application
@@ -33,20 +33,74 @@ app.ClearOutputWindow()
 # INPUTS
 
 # Define set names
-terminal_set_name = 'Terminals'
+term_set_name = 'Terminals'
 line_set_name = 'Lines'
-transformer_set_name = 'Transformers'
+tx_set_name = 'Transformers'
+cb_set_name = 'Circuit Breakers'
 
 # Define object attributes to collect from load flow results
-terminal_attributes = ['e:uknom', 'm:u', 'm:Ul', 'Ir']
-line_attributes = ['Inom', 'c:loading']
-transformer_attributes = ['Snom', 'c:loading', 'n:u:bushv', 'n:u:buslv', 'c:nntap']
-
-# Define headings for results
-terminal_headings = ['Name', 'Rated Voltage (kV)', 'Voltage (pu)', 'Voltage (kV)', 'Rated Current (kA)']
-line_headings = ['Name', 'Rated Current (kA)', 'Loading (%)']
-transformer_headings = ['Name', 'Rated Power (MVA)', 'Loading (%)',
-                        'HV Voltage (kV)', 'LV Voltage (kV)', 'Tap Position']
+term_dict = {'attributes': ['loc_name',
+                            'e:uknom',
+                            'm:u',
+                            'm:Ul',
+                            'e:vmax',
+                            'e:vmin',
+                            'm:Sout',
+                            'Ir',
+                            'Ithlim',
+                            'Tkr',
+                            'Iplim'],
+               'headings': ['Name',
+                            'Rated Voltage (kV)',
+                            'Voltage (pu)',
+                            'Voltage (kV)',
+                            'Upper Voltage Limit (pu)',
+                            'Lower Voltage Limit (pu)',
+                            'Outgoing Apparent Power (kVA)',
+                            'Rated Current (kA)',
+                            'Rated Short-Time Thermal Current (kA)',
+                            'Rated Short-Circuit Duration (s)',
+                            'Short-Circuit Peak Current (kA)']}
+line_dict = {'attributes': ['loc_name',
+                            'Inom',
+                            'c:loading',
+                            'e:dline',
+                            'm:I:bus2'],
+               'headings': ['Name',
+                            'Rated Current (kA)',
+                            'Loading (%)',
+                            'Line Length (m)',
+                            'Maximum Current (kA)']}
+tx_dict = {'attributes': ['loc_name',
+                          't:utrn_h',
+                          't:utrn_l',
+                          'Snom',
+                          'm:I:buslv',
+                          'c:loading',
+                          'n:u:bushv',
+                          'n:u:buslv',
+                          'c:nntap'],
+             'headings': ['Name',
+                          'Rated Primary Voltage (kV)',
+                          'Rated Secondary Voltage (kV)',
+                          'Rated Power (MVA)',
+                          'Current Secondary (kA)',
+                          'Loading (%)',
+                          'HV Voltage (kV)',
+                          'LV Voltage (kV)',
+                          'Tap Position']}
+cb_dict = {'attributes': ['loc_name',
+                          'Inom',
+                          'm:I:bus1',
+                          'm:I:bus2',
+                          'm:Brkload:bus1',
+                          'm:Brkload:bus2'],
+             'headings': ['Name',
+                          'Rated Current (kA)',
+                          'Current Bus1 (kA)',
+                          'Current Bus2 (kA)',
+                          'Circuit Breaker Loading Bus 1 (%)',
+                          'Circuit Breaker Loading Bus 2 (%)']}
 
 # Define operation scenarios to perform load flow analysis on
 operation_scenarios = ['Maximum Normal Load', 'Peak Load']
@@ -54,9 +108,10 @@ operation_scenarios = ['Maximum Normal Load', 'Peak Load']
 #############################################################
 
 # Initialise dataframes to store results
-all_terminal_results = pd.DataFrame()
+all_term_results = pd.DataFrame()
 all_line_results = pd.DataFrame()
-all_transformer_results = pd.DataFrame()
+all_tx_results = pd.DataFrame()
+all_cb_results = pd.DataFrame()
 
 # Get script path and make results directory
 script_path = os.getcwd()
@@ -71,23 +126,41 @@ for operation_scenario in operation_scenarios:
     LoadFlow(method=0, auto_tap=0, feeder_scaling=0, op_scen=operation_scenario)
 
     # Collect objects data
-    terminal_df = GetData(terminal_set_name, terminal_attributes, result_headings=terminal_headings).result
-    line_df = GetData(line_set_name, line_attributes, result_headings=line_headings).result
-    transformer_df = GetData(transformer_set_name, transformer_attributes, result_headings=transformer_headings).result
+    term_df = GetData(term_set_name, term_dict['attributes']).result
+    line_df = GetData(line_set_name, line_dict['attributes']).result
+    tx_df = GetData(tx_set_name, tx_dict['attributes']).result
+    cb_df = GetData(cb_set_name, cb_dict['attributes']).result
 
     # Insert column with operation scenario name
-    terminal_df.insert(0, 'Scenario', [operation_scenario] * terminal_df.shape[0])
+    term_df.insert(0, 'Scenario', [operation_scenario] * term_df.shape[0])
     line_df.insert(0, 'Scenario', [operation_scenario] * line_df.shape[0])
-    transformer_df.insert(0, 'Scenario', [operation_scenario] * transformer_df.shape[0])
+    tx_df.insert(0, 'Scenario', [operation_scenario] * tx_df.shape[0])
+    cb_df.insert(0, 'Scenario', [operation_scenario] * cb_df.shape[0])
 
     # Append scenario results to master data frames
-    all_terminal_results = pd.concat([all_terminal_results, terminal_df])
+    all_term_results = pd.concat([all_term_results, term_df])
     all_line_results = pd.concat([all_line_results, line_df])
-    all_transformer_results = pd.concat([all_transformer_results, transformer_df])
+    all_tx_results = pd.concat([all_tx_results, tx_df])
+    all_cb_results = pd.concat([all_cb_results, cb_df])
+
+# Processing Data into report tables
+busbar_utilisation = busbar_util(all_term_results)
+busbar_voltage_level = busbar_voltage(all_term_results)
+cable_utilisation = cable_util(all_line_results)
+tx_utilisation = tx_util(all_tx_results)
+cb_utilisation = cb_util(all_cb_results)
 
 # Write results to xlsx workbook in separate sheets
 with pd.ExcelWriter(results_path + 'LoadFlowResults.xlsx') as writer:
-    all_terminal_results.to_excel(writer, sheet_name='Terminal', index=False)
+    all_term_results.to_excel(writer, sheet_name='Terminal', index=False)
     all_line_results.to_excel(writer, sheet_name='Line', index=False)
-    all_transformer_results.to_excel(writer, sheet_name='Transformer', index=False)
+    all_tx_results.to_excel(writer, sheet_name='Transformer', index=False)
+    all_cb_results.to_excel(writer, sheet_name='Circuit Breaker', index=False)
+
+    busbar_utilisation.to_excel(writer, sheet_name='BusbarUtilisation', index=False)
+    busbar_voltage_level.to_excel(writer, sheet_name='BusbarVoltageLevel', index=False)
+    cable_utilisation.to_excel(writer, sheet_name='CableUtilisation', index=False)
+    cb_utilisation.to_excel(writer, sheet_name='CircuitBreakerUtilisation', index=False)
+    tx_utilisation.to_excel(writer, sheet_name='TransformerUtilisation', index=False)
+
     app.PrintPlain('Load flow results written to: %s' % (results_path + 'LoadFlowResults.xlsx'))
